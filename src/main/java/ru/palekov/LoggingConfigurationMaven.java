8f720043722eed69f9c6d4d9ee4c78e82e959037
Palekov Alexander <palekov-2011@mail.ru>
2024-06-17 15:47:36 +0300

    initial commit

---
+++ src/main/java/ru/palekov/LoggingConfigurationMaven.java
@@ -0,0 +1,7 @@
+package ru.palekov;
+
+public class LoggingConfigurationMaven {
+    public static void testLog(String message) {
+        System.out.println("Message from logging-starter-maven: " + message);
+    }
+}
